4b5791e0a9cc10be1cba3ec09e7df1b78a551a49
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2025-02-20 18:02:20 +0900

    관광명소추가

--- src/data/attractionMockData.js
+++ src/data/attractionMockData.js
@@ -1,56 +1,84 @@
+import SeoulTower from "assets/images/seoul-tower.jpg";
+import BukchonHanok from "assets/images/bukchon-hanok.jpg";
+import Gyeongbokgung from "assets/images/gyeongbokgung.jpg";
+import Cheonggyecheon from "assets/images/cheonggyecheon.jpg";
+import DDP from "assets/images/ddp.jpg";
+import NationalMuseum from "assets/images/national-museum.jpg";
+import Deoksugung from "assets/images/deoksugung.jpg";
+import Everland from "assets/images/everland.jpg";
+import BusanCinema from "assets/images/busan-cinema.jpg";
+
 export const attractionMockData = [
   {
     attractionName: "N Seoul Tower",
     region: "Seoul",
-    type: "Tourist Attraction",
-    price: 21000,
+    openTime: "10:00",
+    closeTime: "12:00",
+    price: 50,
+    imageURL: SeoulTower,
   },
   {
     attractionName: "Bukchon Hanok Village",
     region: "Seoul",
-    type: "Historic Site",
-    price: "free",
+    openTime: "09:00",
+    closeTime: "11:30",
+    price: 50,
+    imageURL: BukchonHanok,
   },
   {
     attractionName: "Gyeongbokgung Palace",
     region: "Seoul",
-    type: "Cultural Landmark",
-    price: 3000,
+    openTime: "09:00",
+    closeTime: "12:00",
+    price: 50,
+    imageURL: Gyeongbokgung,
   },
   {
     attractionName: "Cheonggyecheon Stream",
     region: "Seoul",
-    type: "Park",
-    price: "free",
+    openTime: "10:00",
+    closeTime: "12:00",
+    price: 50,
+    imageURL: Cheonggyecheon,
   },
   {
     attractionName: "DDP (Dongdaemun Design Plaza)",
     region: "Seoul",
-    type: "Cultural Center",
-    price: "free",
+    openTime: "10:00",
+    closeTime: "12:30",
+    price: 50,
+    imageURL: DDP,
   },
   {
     attractionName: "National Museum of Korea",
     region: "Seoul",
-    type: "National Museum",
-    price: "free",
+    openTime: "10:00",
+    closeTime: "12:30",
+    price: 50,
+    imageURL: NationalMuseum,
   },
   {
     attractionName: "Deoksugung Palace",
     region: "Seoul",
-    type: "Cultural Landmark",
-    price: 1000,
+    openTime: "09:00",
+    closeTime: "11:30",
+    price: 50,
+    imageURL: Deoksugung,
   },
   {
     attractionName: "Everland",
     region: "Seoul",
-    type: "Theme Park",
-    price: 25000,
+    openTime: "10:00",
+    closeTime: "13:00",
+    price: 50,
+    imageURL: Everland,
   },
   {
     attractionName: "Busan Cinema Center",
     region: "Seoul",
-    type: "Cinema Complex",
-    price: 10000,
+    openTime: "09:00",
+    closeTime: "11:30",
+    price: 50,
+    imageURL: BusanCinema,
   },
 ];

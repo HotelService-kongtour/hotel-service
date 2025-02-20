9385c83023c05c03482c3fc301b8bedff441753b
kimhyoungseok <khs43833@gmail.com>
2025-02-20 15:23:14 +0900

    deign/호텔&예약 내역 변경 목데이터 분리

---
+++ src/data/hotelMockData.js
@@ -0,0 +1,21 @@
+export const hotelMockData = [
+  {
+    hotelName: "그랜드호텔",
+    region: "서울",
+    theme: "Korean-Traditional",
+    availableRooms: 15,
+    reservationCount: 45,
+    hotelNumber: "02-2292-2222",
+    review: null,
+  },
+  {
+    hotelName: "시티호텔",
+    region: "부산",
+    theme: "Pet-friendly",
+    availableRooms: 8,
+    reservationCount: 32,
+    hotelNumber: "02-2292-2223",
+    review: null,
+  },
+  // ... 18개 더 추가 (비슷한 패턴으로 데이터 추가)
+]; 

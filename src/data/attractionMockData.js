d6c9401f57e7fd8203d5497785555c02e8a58f38
hunyaa10 <161268128+hunyaa10@users.noreply.github.com>
2025-02-20 17:07:06 +0900

    관광명소추가

---
+++ src/data/attractionMockData.js
@@ -0,0 +1,56 @@
+export const attractionMockData = [
+  {
+    attractionName: "N Seoul Tower",
+    region: "Seoul",
+    type: "Tourist Attraction",
+    price: 21000,
+  },
+  {
+    attractionName: "Bukchon Hanok Village",
+    region: "Seoul",
+    type: "Historic Site",
+    price: "free",
+  },
+  {
+    attractionName: "Gyeongbokgung Palace",
+    region: "Seoul",
+    type: "Cultural Landmark",
+    price: 3000,
+  },
+  {
+    attractionName: "Cheonggyecheon Stream",
+    region: "Seoul",
+    type: "Park",
+    price: "free",
+  },
+  {
+    attractionName: "DDP (Dongdaemun Design Plaza)",
+    region: "Seoul",
+    type: "Cultural Center",
+    price: "free",
+  },
+  {
+    attractionName: "National Museum of Korea",
+    region: "Seoul",
+    type: "National Museum",
+    price: "free",
+  },
+  {
+    attractionName: "Deoksugung Palace",
+    region: "Seoul",
+    type: "Cultural Landmark",
+    price: 1000,
+  },
+  {
+    attractionName: "Everland",
+    region: "Seoul",
+    type: "Theme Park",
+    price: 25000,
+  },
+  {
+    attractionName: "Busan Cinema Center",
+    region: "Seoul",
+    type: "Cinema Complex",
+    price: 10000,
+  },
+];
